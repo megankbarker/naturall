dce0b0129964ff5f98915216db0695c4c2e1c4b8
rosebuddy <helloladybread@gmail.com>
2016-01-26 10:33:37 +0000

    Add about page and copy text

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root 'sightings#index'
+  get 'about', to: 'sightings#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
