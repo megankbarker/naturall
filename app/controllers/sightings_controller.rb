e16afb7ddce170d88c15ede42e93412b597b9e76
rosebuddy <helloladybread@gmail.com>
2016-01-28 18:47:39 +0000

    Make submit button create a new place

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -6,4 +6,15 @@ class SightingsController < ApplicationController
 	def new
 		@sighting = Sighting.new
 	end
+
+	def create
+		Sighting.create(sighting_params) # sends item to database
+		redirect_to root_path
+	end
+
+	private
+
+	def sighting_params # pulls info from sighting form
+		params.require(:sighting).permit(:species, :description, :address)
+	end
 end
